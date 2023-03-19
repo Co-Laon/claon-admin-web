660712e663d2ea5a50dbc10e6ed309dca4a81bea
jinwoo <qowlsdn8007@gmail.com>
2023-03-19 17:16:20 +0900

    chore: init mui & emotion

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    emotion: {
+      sourceMap: true,
+      autoLabel: 'always', // css 명을 자동으로 붙여줌
+      labelFormat: '[filename]--[local]',
+    }
+  }
 }
 
 module.exports = nextConfig
