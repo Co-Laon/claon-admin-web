bb9b6b40ecf37f69d735d589274e9b7c9913b5e3
qowlsdn8007 <qowlsdn8007@gmail.com>
2023-03-22 22:25:01 +0900

    refac: 기존 코드에 lint 에러 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+/* eslint-disable react/jsx-props-no-spreading */
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
