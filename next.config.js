243577a1db9e3d0f98318e04e1cbfd842df678e9
jinwoo <qowlsdn8007@gmail.com>
2023-03-27 18:51:17 +0900

    chore: emotion 옵션 제거 후 동작 확인

--- next.config.js
+++ next.config.js
@@ -38,11 +38,7 @@ const securityHeaders = [
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
-    emotion: {
-      sourceMap: true,
-      autoLabel: 'always', // css 명을 자동으로 붙여줌
-      labelFormat: '[filename]--[local]',
-    },
+    emotion: true,
     // prod 환경에서 에러 이외의 콘솔 제거
     removeConsole: isProd
       ? {
