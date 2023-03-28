10fd35e9b2e12494d31b13beb7fd6322c653864c
qowlsdn8007 <33804074+qowlsdn8007@users.noreply.github.com>
2023-03-28 10:02:48 +0900

    chore: lint, prettier 적용 (#6)
    * chore: lint, prettier 적용
    
    * refac: 기존 코드에 lint 에러 제거
    
    * chore: eslint rule에 prettier endofline 관련 룰 추가
    
    * style: lint에 맞게 코드 적용
    
    * chore: emotion 옵션 제거 후 동작 확인

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    arrowParens: "always",
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    printWidth: 80,
+    proseWrap: "always",
+    quoteProps: "as-needed",
+    semi: true,
+    singleQuote: true,
+    trailingComma: "es5",
+    useTabs: false,
+    tabWidth: 2
+};
