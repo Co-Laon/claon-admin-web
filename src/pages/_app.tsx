629a32fc1efb3f26de3bf513cfa7e106bb082ffd
jinwoo <qowlsdn8007@gmail.com>
2023-03-28 11:11:54 +0900

    chore: react-hook-form, recoil 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
