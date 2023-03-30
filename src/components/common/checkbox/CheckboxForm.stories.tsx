0cc372e9eeea0b3b717fe4870bd9b7b8ca34d5eb
qowlsdn8007 <33804074+qowlsdn8007@users.noreply.github.com>
2023-03-30 20:00:17 +0900

    feat: 공통 컴포넌트 스토리북 생성 (#21)
    * chore: storybook webpack에 svg loader 설정, default path 설정
    
    * feat: 공통 컴포넌트들 스토리 파일 생성

---
+++ src/components/common/checkbox/CheckboxForm.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryFn } from '@storybook/react';
+import { useForm } from 'react-hook-form';
+import CheckboxForm from './CheckboxForm';
+import { CheckboxProps } from './type';
+
+const meta: Meta<typeof CheckboxForm> = {
+  title: '공통/CheckboxForm',
+  component: CheckboxForm,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <div style={{ width: '100%', height: '100%', padding: '20px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryFn<typeof CheckboxForm>;
+
+function Template(args: CheckboxProps) {
+  const { register } = useForm();
+  return <CheckboxForm {...args} formKey="check" register={register} />;
+}
+
+export const Primary: Story = Template.bind({});
+Primary.args = {
+  label: 'Checkbox',
+  formKey: 'checkbox',
+};
