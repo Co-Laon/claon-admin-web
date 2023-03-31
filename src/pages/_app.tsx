b8356d9ed2d0da674d8d0ce6c956cebed4b29a51
qowlsdn8007 <33804074+qowlsdn8007@users.noreply.github.com>
2023-03-31 18:32:56 +0900

    feat: 로그인 페이지 뷰 구현 (#22)
    * feat: login 페이지 마크다운 구성
    
    * chore: webpack에 svg 설정 적용
    
    * feat: 로그인 페이지 스타일 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from 'react-query';
