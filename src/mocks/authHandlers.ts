45ad5cda6e81404e40842066ef5bedf72f980b6a
qowlsdn8007 <qowlsdn8007@gmail.com>
2023-04-10 16:42:00 +0900

    feat: auth, center 관련 모킹 api 핸들러 구현

--- src/mocks/authHandlers.ts
+++ src/mocks/authHandlers.ts
@@ -1,20 +1,51 @@
 import { rest } from 'msw';
 import { SignInResponse } from '@/features/Login';
+import {
+  LectorRegister,
+  ManagerRegister,
+  RegisterResponse,
+} from '@/features/Register';
+import { nickNameStore } from './mockData';
 
 const authHandler = [
+  // 로그인
   rest.post('/auth/:provider/sign-in', (req, res, ctx) => {
-    console.log('req: ', req);
     const response: SignInResponse = {
       access_token: 'access_token',
       refresh_token: 'refresh_token',
       is_signed_up: false,
-      profile: {
-        profile_image: 'profile_image',
-        email: 'email',
-        nickname: 'nickname',
-        instagram_nickname: 'instagram_nickname',
-        role: 'lector',
-      },
+    };
+    return res(ctx.json(response));
+  }),
+
+  // center 회원가입
+  rest.post<ManagerRegister>('/auth/center/sign-up', async (req, res, ctx) => {
+    const { profile } = await req.json<ManagerRegister>();
+
+    const response: RegisterResponse = {
+      is_proofed: true,
+      profile,
+    };
+    return res(ctx.json(response));
+  }),
+
+  // lector 회원가입
+  rest.post<LectorRegister>('/auth/lector/sign-up', async (req, res, ctx) => {
+    const { profile } = await req.json<LectorRegister>();
+
+    const response: RegisterResponse = {
+      is_proofed: true,
+      profile,
+    };
+    return res(ctx.json(response));
+  }),
+
+  // nickname 중복 체크
+  rest.get('/auth/nickname/:nickname/is-duplicated', (req, res, ctx) => {
+    const { nickname } = req.params;
+    const isDuplicated = nickNameStore.has(`${nickname}`);
+    const response = {
+      is_duplicated: isDuplicated,
     };
     return res(ctx.json(response));
   }),
