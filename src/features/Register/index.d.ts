0e0f4641dfac7c739cd4768c08f2be86179b4961
qowlsdn8007 <qowlsdn8007@gmail.com>
2023-04-10 16:27:05 +0900

    refac: type 네이밍, 분리

--- src/features/Register/index.d.ts
+++ src/features/Register/index.d.ts
@@ -1,5 +1,11 @@
 import { Career, Certificate, Contest } from '@/features/common/types/lector';
+import { Center } from '@/features/common/types/center';
 import { Profile } from '@/features/common/types/profile';
+
+export interface RegisterResponse {
+  is_proofed: boolean;
+  profile: Profile;
+}
 
 export interface LectorRegister {
   profile: Profile;
@@ -8,4 +14,9 @@ export interface LectorRegister {
   certificate_list?: Certificate[];
   career_list?: Career[];
   proof_list?: string[];
+}
+
+export interface ManagerRegister extends Center {
+  profile: Profile;
+  proof_list: string[];
 }
