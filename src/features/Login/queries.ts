8571faca2b0762341353233fd3e58e86f0e76136
qowlsdn8007 <qowlsdn8007@gmail.com>
2023-04-11 13:30:39 +0900

    comment: login 관련 mutation 동작 설명 추가

--- src/features/Login/queries.ts
+++ src/features/Login/queries.ts
@@ -39,13 +39,14 @@ export const useOAuthSignIn = () => {
     data: userData,
     isLoading: signInClaonLoading,
   } = useMutation(postSignIn, {
-    // 성공 시 가입 유무 판단 후 라우팅
-    // 회원가입 => 이메일 파라미터 전달
     onSuccess: (res) => {
-      // eslint-disable-next-line no-console
+      // 성공 시  토큰 axios에 디폴트 설정
       const { access_token: accessToken, refresh_token: refreshToken } = res;
       axios.defaults.headers.common['access-token'] = accessToken;
       axios.defaults.headers.common['refresh-token'] = refreshToken;
+
+      // 성공 시 가입 유무 판단 후 라우팅
+      // 회원가입 => 이메일 파라미터 전달
       const registerPath = `/register?email=${res.profile?.email}`;
       const homePath = '/home';
       const nextPath = res.is_signed_up ? homePath : registerPath;
