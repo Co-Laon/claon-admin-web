8a0b05322a3ab481651aeac92feb179d76b27d87
qowlsdn8007 <qowlsdn8007@gmail.com>
2023-05-16 20:22:53 +0900

    fix: change mock-server start condition

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-const mockingUrl = 'http://localhost:3000';
 // axios header set 함수
 export const axiosSetHeader = (accessToken?: string, refreshToken?: string) => {
   axios.defaults.headers.common['access-token'] = accessToken;
@@ -10,9 +9,9 @@ export const axiosSetHeader = (accessToken?: string, refreshToken?: string) => {
 // axios 디폴트 설정,  url, token 만료 시 자동 갱신에 사용
 export const axiosConfig = () => {
   axios.defaults.baseURL =
-    process.env.NODE_ENV === 'production'
-      ? `${process.env.NEXT_PUBLIC_API}`
-      : mockingUrl;
+    process.env.NEXT_PUBLIC_MOCK_SERVER === 'true'
+      ? process.env.NEXT_PUBLIC_MOCK_SERVER_API
+      : process.env.NEXT_PUBLIC_API;
   axios.interceptors.response.use((response) => {
     if ('access-token' in response.headers) {
       const accessToken = response.headers['access-token'];
